7d31ce82ae99481b2d853e35d4cbe25d30706a54
Ajob Kustra <ajobkustra.p@gmail.com>
2021-01-10 15:27:27 +0100

    Commented movement attempt with cosine and sine

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import pgzrun
 import pygame
 from detect_mask_video import MaskDetector
 import threading
+import math
 
 WIDTH = 700 # width of the window
 HEIGHT = 800 # height of the window 
@@ -10,12 +11,14 @@ HEIGHT = 800 # height of the window
 car = Actor("racecar2")
 car.pos = 350, 560
 speed = 2
+direction_speed_multiplier = [0, 1]
 gassing = False
 # TODO: Put direction in this variable
 direction = None # should be "LEFT", "RIGHT" or None
 
 def is_gassing():
     global gassing
+
     gassing = md.is_gassing()
     threading.Timer(0.3, is_gassing).start()
 
@@ -30,8 +33,21 @@ def draw():
 def update(): 
     global gassing, speed, WIDTH, HEIGHT
 
-    if gassing and car.y + speed > 0: car.y -= speed
-    elif car.y - speed < HEIGHT: car.y += speed
+    # Attempt to rotation using sine and cosine
+    # print(car.angle)
+
+    # if (keyboard.left):
+    #     car.angle += 1
+    # elif (keyboard.right):
+    #     car.angle -= 1
+
+    # direction_speed_multiplier[0] = math.cos(car.angle / 50)
+    # direction_speed_multiplier[1] = math.sin(car.angle / 50)
+
+    if gassing and car.y + speed > 0: car.y -= speed * direction_speed_multiplier[1]
+    elif car.y - speed < HEIGHT: car.y += speed * direction_speed_multiplier[1]
+
+    car.x += direction_speed_multiplier[0] * speed
 
 md = MaskDetector()
 
